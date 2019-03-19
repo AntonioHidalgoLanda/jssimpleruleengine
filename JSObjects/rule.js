a76d981ec17a662cc85d4a6f4c499730cf100a17
AntonioHidalgoLanda <hidalgo.landa@gmail.com>
2019-03-19 21:10:46 +0000

    first commit

---
+++ JSObjects/rule.js
@@ -0,0 +1,162 @@
+/* This code requires serious refactoring */
+/*jslint devel: true */
+/* eslint-disable no-console */
+
+/*global
+console
+*/
+function Rule (strCondition, strReaction) {
+    // private variables (TODO - make them private)
+    this.condition;
+    this.reaction;
+    
+    // Static variables
+    this.regExAssignation = /[^<>=]=[^=<>]/gm;
+    this.regExQuote = new RegExp("\"+","gm"); //("\"*","gm")
+    this.regExScope = new RegExp("[A-Za-z]([\\.]*\\w)*","gm");
+    this.replaceScope = "scope.$&";
+    
+    this.target = [];
+    
+    // creation logic
+    this.setCondition(strCondition);
+    this.setReaction(strReaction);
+}
+
+Rule.prototype.setCondition = function (str) {
+    this.condition = "";
+    if (this.regExAssignation.test(str)) {
+        console.log("Rule conditionscannot use assignations.");
+        return this;
+    }
+    
+    // Don't tolerate strings, strings will be part of facts
+    if (this.regExQuote.test(str)) {
+        console.log("Rule conditions and reaction cannot use quotes.");
+        return this;
+    }
+    this.condition = str.replace(this.regExScope,this.replaceScope);
+    
+    return this;
+}
+
+Rule.prototype.setReaction = function (str) {
+    // Don't tolerate strings, strings will be part of facts
+    this.reaction = "";
+    if (this.regExQuote.test(str)) {
+        console.log("Rule conditions and reaction cannot use quotes.");
+        return this;
+    }
+    
+    this.reaction = str.replace(this.regExScope,this.replaceScope);
+    
+    return this;
+}
+
+Rule.prototype.execute = function (facts, candidate) {
+    var candidates;
+    if (candidate) {
+        candidates = [];
+        candidates.push(candidate);
+    }
+    else {
+        candidates = this.getCandidates(facts);
+    }
+    
+    for (var item in candidates) {
+        var scope = {};                 // clean up any scope variable// scope = []
+        this.assignTargets(candidates[item], scope);   // scope[target] = candidate[target] || break
+        if (eval(this.condition)) {
+                eval(this.reaction);
+                this.committ(facts, scope);       // candidate[target] = scope[target]
+        }
+            
+    }
+    return this;
+};
+
+// Not working - Creates duplicates....
+Rule.prototype.getCandidates = function(facts){
+    var targets = this.extractTargets();    // All the different entities in rule.condition and rule.reaction
+    // All possible combinations for the target of that rule
+    var candidates = [];
+    var candidate = {};
+    for (var target in targets){
+        var attributes = targets[target];
+        for (var itemName in facts) {
+            var item = facts[itemName];
+            var elegible = true;
+            for (var attribute in attributes){
+                if (!item.hasOwnProperty(attributes[attribute])){
+                    elegible = false;
+                    break;
+                }
+            }
+            if (elegible) {
+                candidate[target] = item;
+                
+                // if lenghs are right
+                if (Object.keys(candidate).length >= Object.keys(targets).length) {
+                    candidates.push(candidate);
+                    candidate=candidate.deepCopy();  //NOT Implemented
+                    console.log(this);
+                    console.log("not implemented");
+                }
+            }
+        }
+    }
+    return candidates;
+};
+
+
+Rule.prototype.assignTargets = function(candidate, scope){
+    for (var target in candidate) {
+        scope[target] = candidate[target];
+    }
+    return this;
+};
+
+Rule.prototype.committ = function(facts, scope){
+    for (var target in scope) {
+        facts[target] = scope[target];
+    }
+    return this;
+};
+
+
+// All the different entities in rule.condition and rule.reaction
+// Use RegEx to get anything with the form scope.XXXXX
+Rule.prototype.extractTargets = function(){
+    var regExTargets = /scope\.\w+/gm;
+    this.target = {};
+    var m;
+    var allString = this.condition + " " + this.reaction;
+    do {
+        m = regExTargets.exec(allString);
+        
+        if (m) {
+            var targetName = m[0].split(".")[1];    //grabbing only the right side after scope.
+            if (!this.target.hasOwnProperty(targetName)) {
+                this.target[targetName] = [];
+            }
+        }
+    } while (m);
+    
+    for (targetName in this.target){
+        var reAttributes = new RegExp("scope\\."+targetName+"\\.\\w+","gm");
+        do {
+            m = reAttributes.exec(allString);
+            if (m) {
+                var attribute = m[0].split(".")[2];    //grabbing only the right side after scope.
+                if (attribute !== null && this.target.hasOwnProperty(targetName)) {
+                    if (!this.target[targetName].hasOwnProperty(attribute)){
+                        this.target[targetName].push(attribute);
+                    }
+                }
+            }
+        } while (m);
+    }
+    
+    return this.target;
+};
+
---
+++ tests/testRule.js
@@ -0,0 +1,68 @@
+/*jslint devel: true */
+/* eslint-disable no-console */
+
+/*global Rule*/
+
+/*global
+console
+*/
+
+var printRule = function (facts, rule) {
+    "use strict";
+    console.log("Fatcs:");
+    console.log(JSON.stringify(facts));
+    console.log("Rule:");
+    console.log(JSON.stringify(rule));
+    
+};
+
+var condition = "";
+var reaction = "";
+var fact = {};
+
+fact.test001_0A = {"value": 2, "message": ""};
+fact.test001_0B = {"value": 7, "message": ""};
+fact.msg = {"msg_rule_triggered": "Rule Triggered"};
+
+
+// Test 1
+condition = "P.value == \"5\"";
+reaction = "P.message = \"rule triggered\"";
+console.log("#test: Rule(" + condition + ", " + reaction + ")");
+var rule = new Rule(condition, reaction);
+printRule(fact, rule);
+console.log(rule.condition === "");
+console.log(rule.reaction === "");
+
+
+condition = "P.value = 5";
+console.log("#test: Rule(" + condition + ", " + reaction + ")");
+rule.setCondition(condition);
+printRule(fact, rule);
+console.log(rule.condition === "");
+
+
+condition = "P.value >= 5";
+reaction = "P.message = msg.msg_rule_triggered";
+console.log("#test: Rule(" + condition + ", " + reaction + ")");
+rule.setCondition(condition).setReaction(reaction);
+printRule(fact, rule);
+console.log(rule.condition !== "");
+console.log(rule.reaction !== "");
+console.log(rule.extractTargets());
+
+
+
+
+console.log("#Execution: P: fact.test001_0A, msg: fact.msg");
+printRule(fact, rule);
+rule.execute(fact, {"P": fact.test001_0A, "msg": fact.msg});
+printRule(fact, rule);
+
+
+console.log("#Execution: P: fact.test001_0B, msg: fact.msg");
+printRule(fact, rule);
+rule.execute(fact, {"P": fact.test001_0B, "msg": fact.msg});
+printRule(fact, rule);
+
+
